91bafc421fb3daf71bd98e911da3d429a2706abc
DWARF-Markus <mbh@dwarf.dk>
2020-09-10 11:05:26 +0200

    clean vue boilerplate ready to go

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Order from '../views/Order.vue';
 
 const routes = [
 	{
@@ -10,11 +11,7 @@ const routes = [
 	{
 		path: '/order',
 		name: 'Order',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(/* webpackChunkName: "about" */ '../views/About.vue'),
+		component: Order,
 	},
 ];
 
